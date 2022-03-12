c945f7860dae6c2b42542268f8e62cdb61f04420
luohua <luoh@veda.com>
2022-03-12 20:46:39 +0800

    first commit

---
+++ src/App.php
@@ -0,0 +1,15 @@
+<?php
+namespace Dream\FrameWork;
+use Dream\Container\Container;
+class App extends Container
+{
+	public function __construct()
+	{
+
+	}
+
+	public function test()
+	{
+		echo 11;
+	}
+}
